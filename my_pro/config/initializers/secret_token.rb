ea4152b4c543fda7e02097583939872065bbee4a
sri-m <srinivaspkl@gmail.com>
2013-08-04 12:27:08 +0530

    First Committing

---
+++ my_pro/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyPro::Application.config.secret_key_base = 'b9737806958f697471caae6bbd60c9025ec01585fffe9e16fbe98373c84db8b928b2a53815c41ceac9069b62b54dcd9b843c24445a26af9d231f8b4e141e2d91'
